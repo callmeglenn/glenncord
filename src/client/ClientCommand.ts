776c4c1a2bca06ad9da016cb286732c18d234610
callmeglenn <glenn.marcell.tjandra@gmail.com>
2022-08-29 16:22:31 +1000

    Set default value of ClientCommand#type to ClientCommandType#DM_ALLOWED

--- src/client/ClientCommand.ts
+++ src/client/ClientCommand.ts
@@ -4,13 +4,13 @@ class ClientCommand implements BaseCommand {
 	public data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder
 	public cooldown?: number
 	public cooldowns?: Collection<string, number>
-	public type: ClientCommandType = ClientCommandType.DM_ALLOWED
+	public type: ClientCommandType
 	public run: Run
 	constructor(params: BaseCommand) {
 		this.data = params.data
 		this.cooldown = params.cooldown ?? null
 		this.cooldowns = this.cooldown ? new Collection() : null
-		if (params.type) this.type = params.type
+		this.type = params.type ?? ClientCommandType.DM_ALLOWED
 		this.run = params.run
 	}
 }
